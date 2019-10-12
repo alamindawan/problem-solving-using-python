9b45ab7232d5674f868335fcf8f17d4d54558fc3
Alamin <alamin@sizramsolutions.com>
2019-10-12 19:40:02 +0600

    password correct problem solved in uri

--- URI/password_correct.py
+++ URI/password_correct.py
@@ -1,7 +1,10 @@
 correct = 2002
-num = int(input())
+n = 0
+while n != correct:
+    num = int(input())
 
-if num == correct:
-    print('Acesso Permitido')
-else:
-    print('Senha Invalida')
+    if num == correct:
+        print('Acesso Permitido')
+        n = num
+    else:
+        print('Senha Invalida')
